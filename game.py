01bf3c4288ed460aacce8d34009692e0fce12812
JudieKhater <judiekhater09@gmail.com>
2024-11-07 16:37:13 -0500

    Added game functionality

--- game.py
+++ game.py
@@ -1,1 +1,26 @@
-print("hello world")
+import random 
+
+number = random.randint(1,500)
+
+##prompt the user to pick a number
+guessNum = 0
+
+while (guessNum < 10):
+    print("Pick a number between 1-500")
+    curr_guess = int(input())
+
+    if curr_guess > number:
+        print ("Guess lower")
+    
+    elif curr_guess < number: 
+        print ("Guess higher")
+
+    else:
+        print("You guessed correct!")
+        break
+
+    guessNum += 1
+
+if (guessNum == 10 and curr_guess != number):
+    print("haha you lost better luck next time")
+    print("The correct number was", number)
